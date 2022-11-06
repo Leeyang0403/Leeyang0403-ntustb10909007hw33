9ed11c0d274d86d69d082eb903cb95ae4cd3ffc0
李揚 <leeyang0403@gmail.com>
2022-11-06 15:15:01 +0800

    last commit

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask , render_template
 import urllib.request as request
 import json
+#import requests
+#import csv
 
 
 app = Flask(__name__)
 
-src="https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+src="https://ods.railway.gov.tw/tra-ods-web/ods/download/dataResource/8ae4cabf6973990e0169947ed32454b9"
+    #如果用open的方式最後還要close，如果沒close會佔記憶體空間
 with request.urlopen(src) as response:
     data = json.load(response)
 
-    for station in data:
-        print(station["ar"])
-    # data = response.read().decode(encoding='utf-8')
-#print(data)
+    #for station in data:
+        #print(station["trnOpDate"])
+
+
 
 @app.route('/')
 def index():
-    return render_template('bike_list.html', data = data)
+    
+    return render_template("bike_list.html", data = data)
 
 if __name__ == '__main__':
     app.run()
